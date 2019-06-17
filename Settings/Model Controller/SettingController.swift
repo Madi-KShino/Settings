f2fa46e97fc8c0bf2df28e3e46a4a43f52928285
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-06-17 11:23:31 -0600

    Completed Model Controller

--- Settings/Model Controller/SettingController.swift
+++ Settings/Model Controller/SettingController.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class SettingController {
+ 
+    //Singleton - best when the array we are accessing may change by user editing.
+    static let sharedInstance = SettingController()
+ 
+    //Could make static (instead of singleton) if absolutely nothing about this array could be changed
+    let settings: [Setting] = {
+        
+        let music = Setting(appName: "iTunes", appIcon: #imageLiteral(resourceName: "iTune"), isOn: false)
+        let appStore = Setting(appName: "App Store", appIcon: #imageLiteral(resourceName: "appStore"), isOn: false)
+        let iBooks = Setting(appName: "iBooks", appIcon: #imageLiteral(resourceName: "iBook"), isOn: true)
+        
+        return [music, appStore, iBooks]
+    }()
+    
+    func toggleIsOn(for setting: Setting) {
+        //checking current setting, and assigning to opposite
+        setting.isOn = !setting.isOn
+    }
+}
